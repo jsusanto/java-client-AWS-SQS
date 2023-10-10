ac20e322a78697a6ae9b6fe9bf4737bfdf31d20d
Jeffry Susanto <jeffry.susanto@hotmail.com>
2023-10-10 16:25:02 +1100

    Add LISTING QUEUES code to AWS SQS

--- java-messaging/src/main/java/java_jms_client.java
+++ java-messaging/src/main/java/java_jms_client.java
@@ -21,6 +21,8 @@ import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.AmazonSQSException;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.ListQueuesRequest;
+import com.amazonaws.services.sqs.model.ListQueuesResult;
 
 public class java_jms_client {
 	public static void main(String[] args) throws JMSException {
@@ -43,6 +45,13 @@ public class java_jms_client {
 	    //****************************************************************************************
 	    /*
 	     * CREATE A NEW QUEUE CALLED 'TEST' in the AWS SQS
+	     *
+	     * Reference: https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/examples-sqs-message-queues.html
+	     *
+	     * import com.amazonaws.services.sqs.AmazonSQS;
+	     * import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+	     * import com.amazonaws.services.sqs.model.AmazonSQSException;
+	     * import com.amazonaws.services.sqs.model.CreateQueueRequest;
 	     */
 	    AmazonSQS sqs = AmazonSQSClientBuilder.standard()
 	            .withCredentials(credentialsProvider)
@@ -50,7 +59,7 @@ public class java_jms_client {
 	            .build();
 
 	    //CREATE A NEW QUEUE
-	    String QUEUE_NAME = "test";
+	    String QUEUE_NAME = "Queue_test";
 	    CreateQueueRequest create_request = new CreateQueueRequest(QUEUE_NAME)
 	            .addAttributesEntry("DelaySeconds", "60")
 	            .addAttributesEntry("MessageRetentionPeriod", "86400");
@@ -64,6 +73,33 @@ public class java_jms_client {
 	    }
 
 	    //****************************************************************************************
+	    /*
+	     * LISTING QUEUES
+	     * Reference: https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/examples-sqs-message-queues.html
+	     *
+	     * import com.amazonaws.services.sqs.AmazonSQS;
+	     * import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+	     * import com.amazonaws.services.sqs.model.ListQueuesResult;
+	     * */
+
+	    //AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+	    ListQueuesResult lq_result = sqs.listQueues();
+	    System.out.println("Your SQS Queue URLs:");
+	    for (String url : lq_result.getQueueUrls()) {
+	        System.out.println(url);
+	    }
+
+	    /*
+	     * Using the listQueues overload without any parameters returns all queues.
+	     * You can filter the returned results by passing it a ListQueuesRequest object.
+	     * */
+	    //AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+	    String name_prefix = "Queue";
+	    lq_result = sqs.listQueues(new ListQueuesRequest(name_prefix));
+	    System.out.println("Queue URLs with prefix: " + name_prefix);
+	    for (String url : lq_result.getQueueUrls()) {
+	        System.out.println(url);
+	    }
 
 	    //Console output to show that the above codes executed
 	    System.out.println("HelloWorld");
